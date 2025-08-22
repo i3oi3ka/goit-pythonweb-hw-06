c34224c997ae179936a3657449eceec97f0d9446
i3oi3ka <i3oi3ka@ukr.net>
2025-08-22 23:25:34 +0300

    Add Student model with SQLAlchemy mapping

---
+++ models/student.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+from db.base import Base
+
+
+class Student(Base):
+    __tablename__ = "students"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String)
+
+    def __repr__(self) -> str:
+        return f"<Student(id={self.id}, name='{self.name}')>"
