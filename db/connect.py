56a37c1f4ed9afd30d13b23be0e84d98b9227fa9
i3oi3ka <i3oi3ka@ukr.net>
2025-08-25 23:55:32 +0300

    Update Docker Compose configuration and database connection string

--- db/connect.py
+++ db/connect.py
@@ -2,7 +2,7 @@ from sqlalchemy.engine import create_engine
 from sqlalchemy.orm import sessionmaker
 
 # url_to_db = "sqlite:///mynotes.db"
-url_to_db = "postgresql+psycopg2:://admin:password@localhost:5432/hw_6_db"
+url_to_db = "postgresql+psycopg://admin:password@localhost:5432/books_db"
 engine = create_engine(url_to_db)
 Session = sessionmaker(bind=engine)
 session = Session()
