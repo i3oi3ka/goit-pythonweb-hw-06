165709c583b0ffe16398026b4faf9644d0c57bd3
i3oi3ka <i3oi3ka@ukr.net>
2025-08-29 23:59:59 +0300

    Implement data seeding functions for students, teachers, subjects, and grades

--- src/seed.py
+++ src/seed.py
@@ -4,7 +4,8 @@ from sqlalchemy.orm import Session
 from sqlalchemy.future import select
 
 from db.connect import url_to_db
-from db.models import Group
+from db.models import Group, Student, Teacher, Subject, Grade
+import random
 
 fake = Faker()
 
@@ -35,5 +36,62 @@ def seed_groups():
         print(f"Data seeding complete. Created {len(new_groups)} new groups.")
 
 
+def seed_students():
+    with Session(engine) as session:
+        groups = session.query(Group.id).all()
+        for _ in range(45):
+            group_id = random.choice(groups)[0]
+            new_student = Student(
+                name=fake.name(), email=fake.email(), group_id=group_id
+            )
+            session.add(new_student)
+            session.commit()
+
+    print(f"Data seeding complete. Created 45 new students.")
+
+
+def seed_teachers():
+    with Session(engine) as session:
+        for _ in range(5):
+            new_teacher = Teacher(name=fake.name(), email=fake.email())
+            session.add(new_teacher)
+            session.commit()
+
+    print(f"Data seeding complete. Created 5 new teachers.")
+
+
+def seed_subjects():
+    subjects = ["Python", "React", "JavaScript", "HTML", "CSS", "SQL", "Java"]
+    with Session(engine) as session:
+        teachers = session.query(Teacher.id).all()
+        for i in range(7):
+            teacher_id = random.choice(teachers)[0]
+            new_subject = Subject(name=subjects[i], teacher_id=teacher_id)
+            session.add(new_subject)
+            session.commit()
+
+    print(f"Data seeding complete. Created 7 new subjects.")
+
+
+def seed_grades():
+    with Session(engine) as session:
+        students = session.query(Student.id).all()
+        subjects = session.query(Subject.id).all()
+        for i in range(20):
+            new_grade = Grade(
+                grade=random.randint(1, 100),
+                student_id=students[i][0],
+                subject_id=random.choice(subjects)[0],
+                date=fake.date_this_year(),
+            )
+            session.add(new_grade)
+            session.commit()
+    print(f"Data seeding complete. Created 20 new foreach students.")
+
+
 if __name__ == "__main__":
     seed_groups()
+    seed_students()
+    seed_teachers()
+    seed_subjects()
+    seed_grades()
